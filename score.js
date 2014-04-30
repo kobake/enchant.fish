1211d83ae8e53c1ad2d81de4ecaffdd52a552153
kobake <kobake@users.sourceforge.net>
2014-04-30 18:54:22 +0900

    Nearest Neighbor

--- score.js
+++ score.js
@@ -5,21 +5,44 @@ Score = function (scene) {
 	this.group = group;
 	this.scene = scene;
 
-	// スプライト
+	// スプライト（数字部分）
 	this.sprites = [];
 	for (var i = 0; i < 6; i++) {
 		var sprite = new Sprite(7, 7);
 		sprite.image = game.assets['img/number7x7-z9.gif'];
-		sprite.x = 320 - 4 - 7 - 10 * i;
+		sprite.x = 320 - 4 - 7 - 18 * (i + 1) - 7;
 		sprite.y = 4;
+		sprite.frame = 2;
+
+		// 拡大
+		sprite.scaleX = 2;
+		sprite.scaleY = 2;
+		sprite.originX = 0;
+		sprite.originY = 0;
+
 		group.addChild(sprite);
 		this.sprites.push(sprite);
 	}
+
+	// スプライト（単位部分）
+	this.sprite_m = new Sprite(7, 7);
+	this.sprite_m.image = game.assets['img/number7x7-z9.gif'];
+	this.sprite_m.x = 320 - 4 - 7 - 18 * 0 - 7;
+	this.sprite_m.y = 4;
+	this.sprite_m.frame = 10;
+
+	// 拡大
+	this.sprite_m.scaleX = 2;
+	this.sprite_m.scaleY = 2;
+	this.sprite_m.originX = 0;
+	this.sprite_m.originY = 0;
+	group.addChild(this.sprite_m);
 
 	// フレーム処理。
 	// 戻り値0: 通常
 	// 戻り値0以外: オブジェクト削除
 	this.frame = function () {
+		// 距離を進める
 		return 0; // 継続
 	};
 
@@ -30,6 +53,8 @@ Score = function (scene) {
 			group.removeChild(sprite);
 		});
 		this.sprites = [];
+		group.removeChild(this.sprite_m);
+		delete this.sprite_m;
 	};
 
 };
