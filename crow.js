ef57acb6d9178ad4c38a4c178dc1eb61f68d79dd
kobake <kobake@users.sourceforge.net>
2014-05-04 03:06:50 +0900

    crow

--- crow.js
+++ crow.js
@@ -1,18 +1,19 @@
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
-// 牛乳パック
+// カラス
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
-// Usage: milk = new Milk(group, 10);
-Milk = function (scene, x) {
-	group = scene.groups[3];
+Crow = function (scene, x, y) {
+	group = scene.groups[5];
 
 	// 参照
 	this.group = group;
 
 	// スプライト
-	this.sprite = new Sprite(64, 120);
-	this.sprite.image = game.assets['img/milk64x120-z3.gif'];
+	this.sprite = new Sprite(80, 80);
+	this.sprite.image = game.assets['img/crow80x80-z5.png'];
 	this.sprite.x = x;
-	this.sprite.y = 240 - 32 - 120;
+	this.sprite.y = y;
+	this.sprite.scaleX = 0.5;
+	this.sprite.scaleY = 0.5;
 	group.addChild(this.sprite);
 
 	// フレーム処理。
@@ -20,11 +21,20 @@ Milk = function (scene, x) {
 	// 戻り値0以外: オブジェクト削除
 	this.frame = function () {
 		this.sprite.x -= LOOP_SPEED;
-		if (this.sprite.x <= -64) {
+		if (this.sprite.x <= -80) {
 			return 1; // 削除
 		}
 		return 0; // 継続
 	};
+	var t = this;
+	this.f = function () {
+		var ret = t.frame();
+		if (ret != 0) {
+			t.dispose();
+			scene.removeEventListener(Event.ENTER_FRAME, t.f);
+		}
+	};
+	scene.addEventListener(Event.ENTER_FRAME, this.f);
 
 	// 削除
 	this.dispose = function () {
@@ -41,16 +51,16 @@ Milk = function (scene, x) {
 	}
 	// ###このへんは基底クラスを作りたい
 	this.getLeft = function () {
-		return this.sprite.x;
+		return this.sprite.x + 5;
 	};
 	this.getRight = function () {
-		return this.sprite.x + this.sprite.width;
+		return this.sprite.x + this.sprite.width - 5;
 	};
 	this.getTop = function () {
-		return this.sprite.y + 4; // 上部4ピクセルの隙間
+		return this.sprite.y + 5;
 	};
 	this.getBottom = function () {
-		return this.sprite.y + this.sprite.height;
+		return this.sprite.y + this.sprite.height - 5;
 	};
 
 	// 当たり判定（相手が getLeft(), getRight() を持っている前提)
@@ -71,9 +81,7 @@ Milk = function (scene, x) {
 	};
 };
 
-// 画像リスト（ぎゅうにゅう）
-Milk.images = [
-	'img/milk64x80-z3.gif',		// 小
-	'img/milk64x120-z3.gif',	// 中
-	'img/milk64x160-z3.gif'		// 大
+// 画像リスト
+Crow.images = [
+	'img/crow80x80-z5.png'
 ];
