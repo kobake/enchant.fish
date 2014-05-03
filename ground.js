4b544b7f02d7564db39d8f59ee1263679f590315
kobake <kobake@users.sourceforge.net>
2014-05-04 02:16:02 +0900

    地形処理

--- ground.js
+++ ground.js
@@ -10,17 +10,19 @@ Ground = function (scene, x) {
 	this.y = 240 - 32;
 
 	// 高さは 1～5
-	this.height = 1; +Math.floor(Math.random() * 5);
+	this.height = 0 + Math.floor(Math.random() * 3);
 
 	// 高さ分だけスプライトを作る
 	this.sprites = [];
 	for (var i = 0; i < this.height; i++) {
 		var sprite = new Sprite(32, 32);
-		sprite.image = game.assets['img/ground32x32.gif'];
+		sprite.image = game.assets['img/ground32x32-z2.gif'];
 		sprite.x = this.x + 100;
 		sprite.y = this.y - (i * 32);
+		sprite.frame = 0;
+		if (i < this.height - 1) sprite.frame = 3;
 		// シーンに追加
-		scene.groups['bg'].addChild(sprite);
+		scene.groups[2].addChild(sprite);
 		// thisに追加
 		this.sprites.push(sprite);
 	}
@@ -45,7 +47,7 @@ Ground = function (scene, x) {
 	var scene = this.scene;
 	this.dispose = function () {
 		_.each(this.sprites, function (sprite) {
-			scene.removeChild(sprite);
+			sprite.parentNode.removeChild(sprite);
 		});
 		this.sprites = [];
 	};
@@ -53,5 +55,5 @@ Ground = function (scene, x) {
 
 // 画像リスト
 Ground.images = [
-	'img/ground32x32.gif' // 地面
+	'img/ground32x32-z2.gif' // 地面
 ];
