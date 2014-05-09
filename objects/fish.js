019e40e11881686476f09a3815d5b98768fb5cb7
kobake <kobake@users.sourceforge.net>
2014-05-09 23:19:50 +0900

    ジャンプ処理委譲整理

--- objects/fish.js
+++ objects/fish.js
@@ -79,58 +79,7 @@ Fish = function (scene) {
 	frames[3] = GenerateFrameArray(31, 24, 1);
 	frames[4] = GenerateFrameArray(39, 32, 1);
 
-	// タッチ開始
-	window.g_touch_skip = 0;
-	scene.addEventListener(Event.TOUCH_START, function (e) {
-		if (window.g_touch_skip == 1) {
-			window.g_touch_skip = 0;
-			return;
-		}
-		console.log("touch start");
-		t.jumpPrepare(); // ジャンプ準備
-	});
-
-	// フレーム処理
-	scene.addEventListener(Event.ENTER_FRAME, function (e) {
-		if (t.charging == 1) {
-			// スプライト切り替え
-			var GetPowerFrame = function (power) {
-				f = 0;
-				if (power < 2) f = 0;
-				else if (power < 6) f = 1;
-				else if (power < 10) f = 2;
-				else if (power < 15) f = 3;
-				else f = 4;
-				return f;
-			};
-			// パワー増強
-			//var old_f = GetPowerFrame(t.power);
-			t.power += 1;
-			var f = GetPowerFrame(t.power);
-			if (f != t.power_frame) {
-				t.power_frame = f;
-				t.sprite.frame = frames[t.power_frame];
-			}
-			//var new_f = GetPowerFrame(t.power);
-			// スプライト切り替え
-			//if (old_f != new_f) {
-			//	t.frame = frames[new_f];
-			//}
-		}
-		else {
-			t.power = 0;
-			if (t.power_frame != 0) {
-				t.power_frame = 0;
-				t.sprite.frame = frames[0];
-			}
-		}
-	});
-
-	// タッチ終了
-	scene.addEventListener(Event.TOUCH_END, function (e) {
-		console.log("touch end");
-		t.jumpGo(); // ジャンプ確定
-	});
+	
 
 	// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
 	// さかなさんのフレーム処理
