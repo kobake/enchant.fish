5509ab1a8edeec442ad77d5b7ab05d44b84fc96e
kobake <kobake@users.sourceforge.net>
2014-05-10 18:05:14 +0900

    バタバタ制御

--- objects/fish_bata.js
+++ objects/fish_bata.js
@@ -1,3 +1,6 @@
+// 速度制限
+LIMIT_MY = 10;
+
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
 // バタ足コントローラ
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
@@ -9,37 +12,8 @@ Fish_Bata.initialize = function (fish) {
 
 	// 各種パラメータ
 	//fish.jumping = 0;
-	//fish.power = 0;
-	//fish.power_frame = 0;
 	//fish.charging = 0;
 	fish.touching = 0;
-
-	// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
-	// ジャンプ処理
-	// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
-	/*
-	fish.jumpPrepare = function () {
-	// チャージ開始
-	if (this.charging == 0) {
-	this.charging = 1;
-	this.power = 0;
-	}
-	};
-	*/
-	/*
-	fish.jumpGo = function () {
-	// ジャンプ
-	if (this.jumping == 0) {
-	this.jumping = 1;
-	this.sprite.my = -16;
-	this.sprite.my = -8 - Math.pow(this.power, 0.6) * 2;
-	}
-
-	// チャージ終了
-	this.charging = 0;
-	this.power = 0;
-	};
-	*/
 
 	// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
 	// イベント
@@ -49,23 +23,46 @@ Fish_Bata.initialize = function (fish) {
 
 	// タッチ開始
 	scene.addEventListener(Event.TOUCH_START, function (e) {
+		if (window.g_touch_skip > 0) {
+			window.g_touch_skip--;
+			return;
+		}
 		console.log("touch start");
 		// 普通のジャンプしてみる
-		self.sprite.my = -16;
+		//self.sprite.my = -16;
+		self.touching = 1;
 	});
 
 	// フレーム処理
-	scene.addEventListener(Event.ENTER_FRAME, function (e) {
-		if (t.touching == 1) {
-			// 上方向の加速度を付ける
+	fish.frameHandlers.push(function (_fish) {
+		// Y方向加速度
+		var ay = 0.9;
+		var ay = 0.7;
+
+		// ユーザ操作による加速度調整
+		if (self.touching == 1) {
+			ay = -0.7;
 		}
 		else {
+		}
+
+		// 加速度による速度計算
+		self.sprite.my += ay;
+		if (self.sprite.my >= LIMIT_MY) self.sprite.my = LIMIT_MY;
+
+		// 速度による位置計算
+		self.sprite.y += self.sprite.my;
+
+		// Y位置制限
+		if (self.sprite.y >= LIMIT_Y) {
+			self.sprite.y = LIMIT_Y;
+			self.sprite.my = 0;
 		}
 	});
 
 	// タッチ終了
 	scene.addEventListener(Event.TOUCH_END, function (e) {
 		console.log("touch end");
-		t.touching = 0;
+		self.touching = 0;
 	});
 };
